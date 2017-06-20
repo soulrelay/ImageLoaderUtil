b5b93258ffd3f4ec11daf6fa333c500b1693ba3f
sushuai <sushuai@didichuxing.com>
2017-06-20 11:28:48 +0800

    调整项目结构

--- mainapp/src/main/java/com/soulrelay/image/MainActivity.java
+++ mainapp/src/main/java/com/soulrelay/image/MainActivity.java
@@ -1,7 +1,7 @@
-package com.baofeng.soulrelay;
+package com.soulrelay.image;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.baofeng.soulrelay.utils.CommonUtils;
@@ -43,3 +43,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
